5f213589663446465ccf226d22b03d20b1ab0d65
lukasj <lukas.jehle@tasman.govt.nz>
2025-06-11 08:57:09 +1200

    add second proof of concept project

---
+++ V2.0/power-bill-planner/src/components/UserInput.vue
@@ -0,0 +1,25 @@
+<template>
+    <form>
+        <label>
+            Current Billing Cycle Estimate
+            <input type="number" v-model="form.currentBillingCycleEstimate" />
+        </label>
+        <label>
+            Next Billing Cycle Estimate
+            <input type="number" v-model="form.nextBillingCycleEstimate" />
+        </label>
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            form: {
+                currentBillingCycleEstimate: '',
+                nextBillingCycleEstimate: ''
+            }
+        }
+    }
+}
+</script>
